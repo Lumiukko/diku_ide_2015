46091bfcad409c69d1a93da9dbd55c59c82159b8
Lumiukko <makko@brainscorch.net>
2015-11-19 21:14:59 +0100

    Started assignment 1 :bird:

---
+++ js/ide.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("body").append('<img id="mysteryImage" src="img/science.gif" />');
+    $("#mysteryImage").css('position', 'fixed');
+    $("#mysteryImage").css('right', '4pt');
+    $("#mysteryImage").css('top', '6pt');
+});
