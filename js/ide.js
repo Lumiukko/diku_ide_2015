f358ad39105f5565e0b703ba274b71fd8f410a20
Lumiukko <makko@brainscorch.net>
2015-11-22 20:31:25 +0100

    Added ToC, changed order

--- js/ide.js
+++ js/ide.js
@@ -3,4 +3,19 @@ $(document).ready(function() {
     $("#mysteryImage").css('position', 'fixed');
     $("#mysteryImage").css('right', '4pt');
     $("#mysteryImage").css('top', '6pt');
+
+
+    // Inspired by: http://www.jankoatwarpspeed.com/automatically-generate-table-of-contents-using-jquery/
+
+    $("body").append('<div id="toc"></div>');
+    $("#toc").append('<span>Table of Contents</span>');
+    $("#toc").append('<ul>');
+    $("h2, h3").each(function(i) {
+        var current = $(this);
+        current.attr("id", "title" + i);
+        $("#toc").append("<li><a id='link" + i + "' href='#title" +
+            i + "' title='" + current.attr("tagName") + "'>" + 
+            current.html() + "</a></li>");
+    });
+    $("#toc").append('</ul>');
 });
