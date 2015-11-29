f024889aa447e544462f104d3b4bd375076314ff
Lumiukko <makko@brainscorch.net>
2015-11-29 16:04:25 +0100

    Added heatmap table example :hotsprings:

--- js/ide_a2_marco.js
+++ js/ide_a2_marco.js
@@ -1,24 +1,57 @@
 $(document).ready(function() { 
     auto_toc();
    
+    var color = d3.scale.linear()
+                  .domain([-10, 5, 30, 900])
+                  .range(["blue", "lightblue", "red", "yellow"]);
+
     var csv = assv2csv("data/ankara_central.txt");
     data = d3.csv.parse(csv);
     //$("#d3js_vis1").append(JSON.stringify(data));
-    show_data(data);
+    //show_data(data);
+    show_heatmap_table(data);
+    
 
-
-
-
-    function show_data(input) {
+    function show_heatmap_table(input) {
         d3.select("#d3js_vis1")
+          .html("<h4>Temperateure in Dregee Celsius in Ankara, Turkey</h4>")
           .append("table")
           .style("border", "1px solid black")
+          .style("text-align", "right")
           .selectAll("tr")
           .data(input)
           .enter()
           .append("tr")
           .html(function(d, i) {
-                return "<td>" + d.YEAR + "</td><td>" + d.JAN + "</td><td>" + d.FEB + "</td>";
+                ret = "";
+                ret += "<td>" + d.YEAR + "</td>";
+                ret += "<td style=\"background-color: " + color(d.JAN) + "\">" + d.JAN + "</td>";
+                ret += "<td style=\"background-color: " + color(d.FEB) + "\">" + d.FEB + "</td>";
+                ret += "<td style=\"background-color: " + color(d.MAR) + "\">" + d.MAR + "</td>";
+                ret += "<td style=\"background-color: " + color(d.APR) + "\">" + d.APR + "</td>";
+                ret += "<td style=\"background-color: " + color(d.MAY) + "\">" + d.MAY + "</td>";
+                ret += "<td style=\"background-color: " + color(d.JUN) + "\">" + d.JUN + "</td>";
+                ret += "<td style=\"background-color: " + color(d.JUL) + "\">" + d.JUL + "</td>";
+                ret += "<td style=\"background-color: " + color(d.AUG) + "\">" + d.AUG + "</td>";
+                ret += "<td style=\"background-color: " + color(d.SEP) + "\">" + d.SEP + "</td>";
+                ret += "<td style=\"background-color: " + color(d.OCT) + "\">" + d.OCT + "</td>";
+                ret += "<td style=\"background-color: " + color(d.NOV) + "\">" + d.NOV + "</td>";
+                ret += "<td style=\"background-color: " + color(d.DEC) + "\">" + d.DEC + "</td>";
+                return ret;
+          });
+    }
+
+    function show_data(input) {
+        d3.select("#d3js_vis1")
+          .append("table")
+          .style("border", "1px solid black")
+          .style("text-align", "right")
+          .selectAll("tr")
+          .data(input)
+          .enter()
+          .append("tr")
+          .html(function(d, i) {
+                return "<td>" + d.YEAR + "</td><td>" + d.JAN + "</td><td>" + d.FEB + "</td><td>" + d.MAR + "</td><td>" + d.APR + "</td><td>" + d.MAY + "</td><td>" + d.JUN + "</td><td>" + d.JUL + "</td><td>" + d.AUG + "</td><td>" + d.SEP + "</td><td>" + d.OCT + "</td><td>" + d.NOV + "</td><td>" + d.DEC + "</td>";
           });
     }
 
@@ -32,10 +65,10 @@ $(document).ready(function() {
                 if (dv.length > 1) {
                     $(dv).each(function(j, value) {
                         if (value != "") {
-                            csv = csv + value + ",";
+                            csv += value + ",";
                         }
                     });
-                    csv = csv + "\n";
+                    csv += "\n";
                 }
             });
         }, "text");
