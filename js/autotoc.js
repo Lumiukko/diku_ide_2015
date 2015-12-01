69570710e9da511c8b6074c9c695c4f026ce7f5f
Lumiukko <makko@brainscorch.net>
2015-12-01 17:14:04 +0100

    separated autotoc, added dummy functions & placeholders

---
+++ js/autotoc.js
@@ -0,0 +1,16 @@
+$(document).ready(function() { 
+    // Auto ToC
+    // Inspired by: http://www.jankoatwarpspeed.com/automatically-generate-table-of-contents-using-jquery/
+
+    $("body").append('<div id="toc"></div>');
+    $("#toc").append('<span>Table of Contents</span>');
+    $("#toc").append('<ul>');
+    $("h2, h3").each(function(i) {
+        var current = $(this);
+        current.attr("id", "title" + i);
+        $("#toc").append("<li><a id='link" + i + "' href='#title" +
+            i + "' title='" + current.attr("tagName") + "'>" + 
+            current.html() + "</a></li>");
+    });
+    $("#toc").append('</ul>');
+});
