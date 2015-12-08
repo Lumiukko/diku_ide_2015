766921b9cd39867cb0ac43a47bffacfef5382530
Lumiukko <makko@brainscorch.net>
2015-12-08 13:37:03 +0100

    Example Geo Visualization of SF Bay Area Counties :earth_americas:
    + some data files

--- js/ide_a4.js
+++ js/ide_a4.js
@@ -1,5 +1,45 @@
 $(document).ready(function() {
+    var w = 600;
+    var h = 400;
+             
+    var projection = d3.geo.orthographic()
+        .scale(12000)
+        .rotate([122.43, -37.78, 0.0])
+        .translate([w/2, h/2])
+        .clipAngle(90)
+        .precision(.1);
     
-    console.log("Stockbrot!");
+    var geopath = d3.geo.path()
+        .projection(projection);
+       
+    var svg = d3.select("#visbox")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h)
+        .style("background-color", "lightblue")
+        .style("border", "1px solid black");
+       
+    var g = svg.append("g");
+    
+    d3.json("data/us-10m.json", function(error, topology) {
+        if (!error) {
+            var world = topojson.object(topology, topology.objects.counties)
+        
+            g.append("path")
+             .datum(world)
+             .attr("d", geopath)
+             .attr("class", "county");
+             
+        }
+        else {
+            console.log(error);
+        }
+    });
+    
+    
+    
+        
+    
 
 });
+
