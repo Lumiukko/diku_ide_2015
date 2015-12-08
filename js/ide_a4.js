c9265e1a2cc5142279b5b9d2420791c79688237d
Lumiukko <makko@brainscorch.net>
2015-12-08 17:47:41 +0100

    Changed the example and added street data :high_brightness:

--- js/ide_a4.js
+++ js/ide_a4.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var h = 400;
              
     var projection = d3.geo.orthographic()
-        .scale(12000)
+        .scale(400000) // Default is 200000, 155000 contains all data points, zoomed in on north east is 400000
         .rotate([122.43, -37.78, 0.0])
         .translate([w/2, h/2])
         .clipAngle(90)
@@ -18,18 +18,46 @@ $(document).ready(function() {
         .attr("height", h)
         .style("background-color", "lightblue")
         .style("border", "1px solid black");
-       
-    var g = svg.append("g");
+
     
-    d3.json("data/us-10m.json", function(error, topology) {
+    d3.json("data/sf_crime.geojson", function(error, topology) {
         if (!error) {
-            var world = topojson.object(topology, topology.objects.counties)
+            //var world = topojson.object(topology, topology.objects.sfcontours); // This is a topojson
+            var world = topology.features;  // This is a normal GeoJSON object
+
+            svg.selectAll("circle")
+               .data(world)
+               .enter()
+               .append("circle")
+               .attr("r", function(d, i) {
+                    if (d.properties.Descript.toLowerCase().indexOf("domestic violence") >= 0) {
+                        return 5;
+                    }
+                    else {
+                        return 2;
+                    }
+               })
+               .attr("cx", function(d, i) {
+                    return projection(d.geometry.coordinates)[0];
+               })
+               .attr("cy", function(d, i) {
+                    return projection(d.geometry.coordinates)[1];
+               })
+               .attr("opacity", 1.0)
+               .attr("fill", function(d, i) {
+                    if (d.properties.Descript.toLowerCase().indexOf("domestic violence") >= 0) {
+                        return "yellow";
+                    }
+                    else {
+                        return "blue";
+                    }
+               })
+               .on("mouseover", function(d, i) {
+                    console.log("Point " + i + ": " + d.properties.Descript);
+               });
+               
+            console.log("FINISHED");
         
-            g.append("path")
-             .datum(world)
-             .attr("d", geopath)
-             .attr("class", "county");
-             
         }
         else {
             console.log(error);
