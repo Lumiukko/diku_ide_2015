a345031ef052e508ad94c3e088ecc6062a899434
Lumiukko <makko@brainscorch.net>
2015-12-08 17:59:21 +0100

    Outfactored the map drawing into a function

--- js/ide_a4.js
+++ js/ide_a4.js
@@ -18,55 +18,73 @@ $(document).ready(function() {
         .attr("height", h)
         .style("background-color", "lightblue")
         .style("border", "1px solid black");
+        
+        
+    draw_map();
 
     
-    d3.json("data/sf_crime.geojson", function(error, topology) {
-        if (!error) {
-            //var world = topojson.object(topology, topology.objects.sfcontours); // This is a topojson
-            var world = topology.features;  // This is a normal GeoJSON object
+    
+    function update_map() {
+        console.log("NOT YET IMPLEMENTED");
+    };
+    
+    
+    function draw_map() {
 
-            svg.selectAll("circle")
-               .data(world)
-               .enter()
-               .append("circle")
-               .attr("r", function(d, i) {
-                    if (d.properties.Descript.toLowerCase().indexOf("domestic violence") >= 0) {
-                        return 5;
-                    }
-                    else {
-                        return 2;
-                    }
-               })
-               .attr("cx", function(d, i) {
-                    return projection(d.geometry.coordinates)[0];
-               })
-               .attr("cy", function(d, i) {
-                    return projection(d.geometry.coordinates)[1];
-               })
-               .attr("opacity", 1.0)
-               .attr("fill", function(d, i) {
-                    if (d.properties.Descript.toLowerCase().indexOf("domestic violence") >= 0) {
-                        return "yellow";
-                    }
-                    else {
-                        return "blue";
-                    }
-               })
-               .on("mouseover", function(d, i) {
-                    console.log("Point " + i + ": " + d.properties.Descript);
-               });
-               
-            console.log("FINISHED");
+        d3.json("data/sfstreets.json", function(error, topology) {
+            if (!error) {
+                
+            }
+            else {
+                console.log(error);
+            }
+        });
         
-        }
-        else {
-            console.log(error);
-        }
-    });
-    
-    
-    
-        
+        d3.json("data/sf_crime.geojson", function(error, topology) {
+            if (!error) {
+                //var world = topojson.object(topology, topology.objects.sfcontours); // This is a topojson
+                var world = topology.features;  // This is a normal GeoJSON object
+
+                svg.selectAll("circle")
+                   .data(world)
+                   .enter()
+                   .append("circle")
+                   .attr("r", function(d, i) {
+                        if (d.properties.Descript.toLowerCase().indexOf("domestic violence") >= 0) {
+                            return 5;
+                        }
+                        else {
+                            return 2;
+                        }
+                   })
+                   .attr("cx", function(d, i) {
+                        return projection(d.geometry.coordinates)[0];
+                   })
+                   .attr("cy", function(d, i) {
+                        return projection(d.geometry.coordinates)[1];
+                   })
+                   .attr("opacity", 1.0)
+                   .attr("fill", function(d, i) {
+                        if (d.properties.Descript.toLowerCase().indexOf("domestic violence") >= 0) {
+                            return "yellow";
+                        }
+                        else {
+                            return "blue";
+                        }
+                   })
+                   .on("mouseover", function(d, i) {
+                        console.log("Point " + i + ": " + d.properties.Descript);
+                   });
+                   
+                console.log("FINISHED");
+            
+            }
+            else {
+                console.log(error);
+            }
+
+        });
+    };
     
 
 });
