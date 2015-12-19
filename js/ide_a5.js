3197fef5420acd483e566e1b86efdc16c4c5e7b4
Lumiukko <makko@brainscorch.net>
2015-12-19 02:02:09 +0100

    See Description! :link:
    + Added bonds only if atoms are less than 1.9 units apart.
    ~ Changed the default molecule to 2RH1.
    ~ Outfactored helpers.
    ~ Outfactored the mouse movements, since they make it impossible to
    render huge molecules.
    # TODO: Properly position the camera, figure out good preset positions
    and angles, or fix the mouse movement issue if possible.

--- js/ide_a5.js
+++ js/ide_a5.js
@@ -5,48 +5,60 @@ $(document).ready(function() {
     var renderer;
     var controls;
     
-    var pdb_loader = new THREE.PDBLoader();
+    var pdb_loader;
     var atoms = [];
     var bonds = [];
     
+    // Enables helpers, i.e. Axis helpers and light helper.
+    var helpers = true;
+   
+    // Enables mouse rotation and zoom.
+    // Warning: This is very very problematic with large molecules!
+    var movement = false;
+    
     
     init();
-    animate();
-    
+    if (movement) animate();
 
     function init() {
         container = $("#visbox1");
         var w = container.width();
         var h = container.height();
         
+        pdb_loader = new THREE.PDBLoader();
+        
         // create a scene, that will hold all our elements such as objects, cameras and lights.
         scene = new THREE.Scene();
         
         // create a camera, which defines where we're looking at.
-        camera = new THREE.PerspectiveCamera(45, w / h, 0.1, 1000);
-        camera.position.set(-20, 20, 130);
+        camera = new THREE.PerspectiveCamera(90, w / h, 1.1, 1000);
+        camera.position.set(130, -80, 10);
         
         // create trackball controls
-        controls = new THREE.TrackballControls( camera, document.getElementById("visbox1") );
-        controls.rotateSpeed = 5.0;
-        controls.zoomSpeed = 0.2;
-        controls.panSpeed = 0.2;
-        controls.noZoom = false;
-		controls.noPan = false;
-        controls.addEventListener( 'change', render);
-
+        if (movement) {
+            controls = new THREE.TrackballControls( camera, document.getElementById("visbox1") );
+            controls.rotateSpeed = 5.0;
+            controls.zoomSpeed = 0.2;
+            controls.panSpeed = 0.2;
+            controls.noZoom = false;
+            controls.noPan = false;
+            controls.addEventListener( 'change', render);
+        };
+        
         // create and setup a renderer
-        renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
+        renderer = new THREE.WebGLRenderer( { antialias: true } );
         renderer.setSize(w, h);
-
+        
         // show axes in the screen
-        var axes = new THREE.AxisHelper(30);
-        scene.add(axes)
+        if (helpers) {
+            var axes = new THREE.AxisHelper(30);
+            scene.add(axes)
+        };
         
-        
+
         /* more of our code here */
-        //load_pdb("data/2RH1.pdb");
-        load_pdb("data/caffeine.pdb");
+        load_pdb("data/2RH1.pdb");
+        //load_pdb("data/caffeine.pdb");
         add_stuff();
       
         
@@ -69,14 +81,19 @@ $(document).ready(function() {
 
     function load_pdb( url ) {
         stretch_factor = 8;
+        sphere_detail = 4;
+        bond_distance_limit = 1.9;
         pdb_loader.load( url, function ( geometry, geometryBonds ) {
               
             //console.log(geometry);
             //console.log(geometryBonds);
             
+            var comparisons = 0;
+            var couples = 0;
+            
             $.each(geometry.vertices, function(i, position) {
                 var atom_color = new THREE.Color().setRGB(geometry.colors[i].r, geometry.colors[i].g, geometry.colors[i].b);
-                var sphereGeometry = new THREE.SphereGeometry(2, 16, 16);
+                var sphereGeometry = new THREE.SphereGeometry(2, sphere_detail, sphere_detail);
                 var sphereMaterial = new THREE.MeshLambertMaterial({color: atom_color, wireframe: false});
                 var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
                 sphere.position.set( position.x * stretch_factor,
@@ -84,10 +101,21 @@ $(document).ready(function() {
                                      position.z * stretch_factor);
                 atoms.push(sphere);
                 scene.add(sphere);
+                
+                $.each(geometry.vertices, function(j, mate_position) {
+                    if (i != j && j > i) {
+                        comparisons++;
+                        if (distance_euclidean(position, mate_position) < bond_distance_limit) {
+                            couples++;
+                            draw_line(position, mate_position, stretch_factor);
+                        }
+                    }
+                });
             });
+            console.log("Number of Comparisons: " + comparisons);
+            console.log("Couples: " + couples);
             
-            
-
+            /*
             for ( var i = 0; i < geometryBonds.vertices.length; i += 2 ) {            
                 var bond_geometry = new THREE.Geometry();
                 bond_geometry.vertices.push(new THREE.Vector3(stretch_factor * geometryBonds.vertices[i].x,
@@ -101,13 +129,27 @@ $(document).ready(function() {
                 bonds.push(bond);
                 scene.add(bond);
             };
-       
-
+            */
+            
             render();
             
         } );
     };
     
+    
+    function draw_line(p1, p2, stretch_factor) {
+        var bond_geometry = new THREE.Geometry();
+        bond_geometry.vertices.push(new THREE.Vector3(stretch_factor * p1.x,
+                                                      stretch_factor * p1.y,
+                                                      stretch_factor * p1.z));
+        bond_geometry.vertices.push(new THREE.Vector3(stretch_factor * p2.x,
+                                                      stretch_factor * p2.y,
+                                                      stretch_factor * p2.z));
+        
+        var bond = new THREE.Line(bond_geometry, new THREE.LineBasicMaterial({ color: "#ffffff" }));
+        bonds.push(bond);
+        scene.add(bond);
+    };
     
     function add_stuff() {
     /*
@@ -131,7 +173,18 @@ $(document).ready(function() {
         var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
         scene.add(light);
         
+        if (helpers) {
+            var lightHelper = new THREE.HemisphereLightHelper(light, 50);
+            scene.add( lightHelper );
+        };
+        
         render();
     };
+    
+    
+    function distance_euclidean(p1, p2) {
+        return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2) + Math.pow(p1.z - p2.z, 2));
+    }
+    
     
 });
