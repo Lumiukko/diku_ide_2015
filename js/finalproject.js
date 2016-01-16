3d12f8c7438b700e8dff42ca9d2cca0211bafb45
Bogdan Pomohaci <pomobc@gmail.com>
2016-01-16 19:09:17 +0100

    Added show/hide button for weapon visualizations

--- js/finalproject.js
+++ js/finalproject.js
@@ -400,159 +400,177 @@ $(document).ready(function() {
     */
     
     /**
-		
-	*/
-	function add_weapon_death_statistics(data){
-		var sides = sort_by_side(data)
-		
-		var ct_data = sides[0];
-		var t_data = sides[1];
-		
-		var ct_weapon = add_weapon_category(ct_data, "killed_by")
-		var t_weapon = add_weapon_category(t_data, "killed_by")
+        
+    */
+    function add_weapon_death_statistics(data){
+        var sides = sort_by_side(data)
+        
+        var ct_data = sides[0];
+        var t_data = sides[1];
+        
+        var ct_weapon = add_weapon_category(ct_data, "killed_by")
+        var t_weapon = add_weapon_category(t_data, "killed_by")
 
-		draw_weapon_bar_chart(ct_weapon, "Counter Terrorists were killed by:", 4, "lightblue");
-		draw_weapon_bar_chart(t_weapon, "Terrorists were killed by:", 4, "pink");
-				
-	}
-	/**
-		
-	*/
-	function add_weapon_fired_statistics(data){
-		var sides = sort_by_side(data)
-		
-		var ct_data = sides[0];
-		var t_data = sides[1];
-		
-		var ct_weapon = add_weapon_category(ct_data, "weapon")
-		var t_weapon = add_weapon_category(t_data, "weapon")
+        draw_weapon_bar_chart(ct_weapon, "Counter Terrorists were killed by:", 4, "lightblue");
+        draw_weapon_bar_chart(t_weapon, "Terrorists were killed by:", 4, "pink");
+                
+    }
+    /**
+        
+    */
+    function add_weapon_fired_statistics(data){
+        var sides = sort_by_side(data)
+        
+        var ct_data = sides[0];
+        var t_data = sides[1];
+        
+        var ct_weapon = add_weapon_category(ct_data, "weapon")
+        var t_weapon = add_weapon_category(t_data, "weapon")
 
-		draw_weapon_bar_chart(ct_weapon, "Counter Terrorists are firing using:", 0.3, "lightblue");
-		draw_weapon_bar_chart(t_weapon, "Terrorists are firing using:", 0.3, "pink");
-				
-	}
+        draw_weapon_bar_chart(ct_weapon, "Counter Terrorists are firing using:", 0.3, "lightblue");
+        draw_weapon_bar_chart(t_weapon, "Terrorists are firing using:", 0.3, "pink");
+                
+    }
 	
-	/**
-		Bar chart of the total number of weapon usage by side
-	*/
-	function draw_weapon_bar_chart(weapon_data, title, factor, colour){
-		var chart_height = weapon_data[0].amount*factor + 25;
-		var chart_width = 1024;
-		var bar_width = 52.5;
-		
-		d3.select("#weapon_visbox")
-						.append("h4")
-						.text(title);
-		
-		var weapon_chart = d3.select("#weapon_visbox")
-							.append("svg")
-							.attr("class", "visbox")
-							.attr("height", chart_height+"px")
-							.attr("width", chart_width+"px");
-		
-		weapon_chart.selectAll(".bar")
-				.data(weapon_data, function(d, i) {return d, i})
-				.enter().append("rect")
-				.attr("class", "bar")
-				.attr("name", function(d) { return (d.name); })
-				.attr("x", function(d, i) {return (i * (bar_width+1)); })
-				.attr("y", function(d) {return chart_height - (d.amount) * factor ;})
+    
+    /**
+        Bar chart of the total number of weapon usage by side
+    */
+    function draw_weapon_bar_chart(weapon_data, title, factor, colour){
+        var chart_height = weapon_data[0].amount*factor + 25;
+        var chart_width = 1024;
+        var bar_width = 52.5;
+        
+        d3.select("#weapon_visbox")
+            .append("h4")
+			.attr("width", "300px")
+			.style("display", "inline-block")
+            .text(title);
+                        
+        var show_chart_btn = d3.select("#weapon_visbox")
+                                .append("button")
+                                .attr("class", "show_chart_btn")
+                                .text("Show chart")
+								.on("click", function(){
+									if (d3.select(this).text() === "Hide chart") {
+										d3.select(this).text("Show chart");
+										weapon_chart.style("display", "none")
+									} else {
+										d3.select(this).text("Hide chart");
+										weapon_chart.style("display", "block")
+									}
+								});
+        
+        var weapon_chart = d3.select("#weapon_visbox")
+                            .append("svg")
+							.style("display", "none")
+                            .attr("class", "visbox")
+                            .attr("height", chart_height+"px")
+                            .attr("width", chart_width+"px");
+        
+        weapon_chart.selectAll(".bar")
+                .data(weapon_data, function(d, i) {return d, i})
+                .enter().append("rect")
+                .attr("class", "bar")
+                .attr("name", function(d) { return (d.name); })
+                .attr("x", function(d, i) {return (i * (bar_width+1)); })
+                .attr("y", function(d) {return chart_height - (d.amount) * factor ;})
                 .attr("width", bar_width+"px")
                 .attr("amount", function(d) { return (d.amount); })
                 .attr("height", function(d) { return (d.amount) * factor; })
                 .attr("stroke", "#000")
-				.attr("fill", colour)
-				
-				//Tooltip
-				.on('mouseover', function(d) {
+                .attr("fill", colour)
+                
+                //Tooltip
+                .on('mouseover', function(d) {
                     tooltip = d3.select("#tooltip_weapon");
                     var mouse_pos = d3.mouse(document.body);
                     tooltip.html("Weapon: " + d.name)
-						.style("opacity", "0")
-						.style("display", "inline")
-						.style("z-index", "1000")
-						.style("left", mouse_pos[0]+"px")
-						.style("top", mouse_pos[1]+"px")
-						.attr("x", mouse_pos)
-						.attr("y", mouse_pos)
-						.transition()
-						.style("opacity", 0.8);
+                        .style("opacity", "0")
+                        .style("display", "inline")
+                        .style("z-index", "1000")
+                        .style("left", mouse_pos[0]+"px")
+                        .style("top", mouse_pos[1]+"px")
+                        .attr("x", mouse_pos)
+                        .attr("y", mouse_pos)
+                        .transition()
+                        .style("opacity", 0.8);
                  })
                  .on("mouseout", function() {
                      d3.select("#tooltip_weapon").style("display", "none");
                  })
-				 
+                 
 
-				
-		weapon_chart.selectAll("text")
-				.data(weapon_data)
-				.enter()
-				.append("text")
-				.text(function(d) {return d.amount;})
-				.attr("text-anchor", "middle")
-				.attr("x", function(d, i) {return i * (bar_width + 1) + (chart_width / 19 ) / 2;})
-				.attr("y", function(d) {return chart_height - (d.amount * factor) - 10;})
-				.attr("font-family", "sans-serif")
-				.attr("font-size", "11px")
-				.attr("fill", "white")
-				
-				//Tooltip
-				.on('mouseover', function(d) {
+                
+        weapon_chart.selectAll("text")
+                .data(weapon_data)
+                .enter()
+                .append("text")
+                .text(function(d) {return d.amount;})
+                .attr("text-anchor", "middle")
+                .attr("x", function(d, i) {return i * (bar_width + 1) + (chart_width / 19 ) / 2;})
+                .attr("y", function(d) {return chart_height - (d.amount * factor) - 10;})
+                .attr("font-family", "sans-serif")
+                .attr("font-size", "11px")
+                .attr("fill", "white")
+                
+                //Tooltip
+                .on('mouseover', function(d) {
                     tooltip = d3.select("#tooltip_weapon");
                     var mouse_pos = d3.mouse(document.body);
                     tooltip.html("Weapon: " + d.name)
-						.style("opacity", "0")
-						.style("display", "inline")
-						.style("z-index", "1000")
-						.style("left", mouse_pos[0]+"px")
-						.style("top", mouse_pos[1]+"px")
-						.attr("x", mouse_pos)
-						.attr("y", mouse_pos)
-						.transition()
-						.style("opacity", 0.8);
+                        .style("opacity", "0")
+                        .style("display", "inline")
+                        .style("z-index", "1000")
+                        .style("left", mouse_pos[0]+"px")
+                        .style("top", mouse_pos[1]+"px")
+                        .attr("x", mouse_pos)
+                        .attr("y", mouse_pos)
+                        .transition()
+                        .style("opacity", 0.8);
                  })
                  .on("mouseout", function() {
                      d3.select("#tooltip_weapon").style("display", "none");
                  })
-	}
-	
-	
-	/**
-		Sorts data by side
-	*/
-	function sort_by_side(data){
-		var ct = [];
-		var t = [];
+    }
+    
+    
+    /**
+        Sorts data by side
+    */
+    function sort_by_side(data){
+        var ct = [];
+        var t = [];
         data.forEach(function(entry) {
-			if(entry.side == "CT") ct.push(entry)
+            if(entry.side == "CT") ct.push(entry)
             else t.push(entry);
         });
-		return [ct, t];
-	}
-	/**
-		Sorts the weapons by usage
-	*/
-	function add_weapon_category(data, object_name){
-		var categories = [];
+        return [ct, t];
+    }
+    /**
+        Sorts the weapons by usage
+    */
+    function add_weapon_category(data, object_name){
+        var categories = [];
         data.forEach(function(entry) {
                 categories.push(entry[object_name]);
         });
-		
-		var unique_weapons = {}; 
-		categories.forEach(function(i) { unique_weapons[i] = (unique_weapons[i] || 0 ) + 1;  });
+        
+        var unique_weapons = {}; 
+        categories.forEach(function(i) { unique_weapons[i] = (unique_weapons[i] || 0 ) + 1;  });
 
-		var sortable = [];
-		for (var n in unique_weapons)
-			  sortable.push([n, unique_weapons[n]])
-		sortable.sort(function(a, b) {return b[1] - a[1]})
-		
-		var sorted_weapons = [];
-		sortable.forEach(function(entry) {
-			sorted_weapons.push({name:entry[0],amount:entry[1]});
+        var sortable = [];
+        for (var n in unique_weapons)
+              sortable.push([n, unique_weapons[n]])
+        sortable.sort(function(a, b) {return b[1] - a[1]})
+        
+        var sorted_weapons = [];
+        sortable.forEach(function(entry) {
+            sorted_weapons.push({name:entry[0],amount:entry[1]});
 
         });
 
-		return sorted_weapons;
-	}
+        return sorted_weapons;
+    }
     
 });
